e432f3d2d0ca5919e2a67f1b1bc422a7886c87af
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-08 21:37:35 +0530

    complete Slider component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
 
 export default function Home() {
     return (
         <div>
             <Announcement/>
             <Navbar/>
+            <Slider/>
         </div>
     )
 }
