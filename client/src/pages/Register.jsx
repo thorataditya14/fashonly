c74b65e70181638c4c5a7370d73787a4ed3adede
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-09 08:43:57 +0530

    complete Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,4 @@
 import styled from 'styled-components'
-import Announcement from '../components/Announcement';
-import Navbar from '../components/Navbar'
-import Products from '../components/Products'
-import Footer from '../components/Footer'
-import NewsLetter from '../components/NewsLetter'
 
 
 const Container = styled.div`
