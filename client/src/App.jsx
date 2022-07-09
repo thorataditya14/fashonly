be1262498b934baa43352a743549cc92424df503
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-09 07:47:28 +0530

    complete ProductList page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+
 
 function App() {
-  return (
-    <>
-      <Home/>
-    </>
-  );
+
+    return <ProductList/>;
+
+    // return (
+    //     <>
+    //         <Home/>
+    //         <ProductList/>
+    //     </>
+    // );
 }
 
 export default App;
