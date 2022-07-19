00a122dd248a875fe0c5720544e8eb571c9f9983
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-19 22:53:04 +0530

    new commit

---
+++ api/routes/auth.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+
+// Register
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString()
+    });
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+        (!user) && res.status(401).json("Invalid credentials");
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user,password,
+            process.env.PASS_SEC
+        );
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        (OriginalPassword !== req.body.password) &&
+            res.status(401).json("Invalid credentials");
+
+        const ( password, ...others ) = user._doc;
+        res.status(200).json(others);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
