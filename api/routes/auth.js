2a1fd01abba69512ca1a3ca5c4d9473420cbc43b
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-22 22:44:36 +0530

    add verifyToken.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 
 // Register
@@ -26,21 +27,30 @@ router.post("/register", async (req, res) => {
 // Login
 router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne({
-            username: req.body.username
-        });
-        (!user) && res.status(401).json("Invalid credentials");
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Invalid credentials");
 
         const hashedPassword = CryptoJS.AES.decrypt(
             user,password,
             process.env.PASS_SEC
         );
+
         const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
         (OriginalPassword !== req.body.password) &&
             res.status(401).json("Invalid credentials");
 
-        const ( password, ...others ) = user._doc;
-        res.status(200).json(others);
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        },
+            process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({...others, accessToken});
     }
     catch (err) {
         res.status(500).json(err);
