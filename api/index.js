4e95b67a6343d12a4d00343c4d3c90aabb480027
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-23 09:50:52 +0530

    add stripe payment

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 
 mongoose.connect(
-    process.env.MONGO_URL
+    process.env.MONGO_URI
 ).then(() => {
     console.log('Database connected successfully');
 }).catch((err) => {
